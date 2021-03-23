db6643e38410c8b23b4dfabbd12675f502cf9459
hafizhsyafiqh <hafizhsyafiqh@gmail.com>
2021-03-23 13:47:22 +0700

    make it better

---
+++ Laravel-profile-main/resources/views/welcome.blade.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Profile Tugas1</title>
+</head>
+
+<body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.0.45/css/materialdesignicons.min.css" integrity="sha256-nRyVCcVDSlWN9d68r9M+rKBLE4k9Cp1j3XSY/umjvvU=" crossorigin="anonymous" />
+    <div class="container">
+        <div class="row mb-3 mt-3">
+            <center>
+                <div class="col-md-6">
+                    <div class="card m-b-30">
+                        <div class="card-header bg-white">
+                            <h5 class="card-title text-black mb-0">Social Profile</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="xp-social-profile">
+                                <div class="xp-social-profile-img">
+                                    <div class="row">
+                                        <div class="col-4 px-1">
+                                            <img src="{{asset('foto/avatar4.jpg') }}" class="rounded img-fluid" alt="img">
+                                        </div>
+                                        <div class="col-4 px-1">
+                                            <img src="{{asset('foto/avatar2.jpg') }}" class="rounded img-fluid" alt="img">
+                                        </div>
+                                        <div class="col-4 px-1">
+                                            <img src="{{asset('foto/avatar3.jpg') }}" class="rounded img-fluid" alt="img">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="xp-social-profile-top">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <div class="xp-social-profile-star py-3">
+                                                <i class="mdi mdi-star font-24"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-6 mt-3">
+                                            <div class="xp-social-profile-avatar text-center">
+                                                <img src="{{asset('foto/avatar1.jpg') }}" alt="user-profile" class="rounded-circle img-fluid">
+                                                <span class="xp-social-profile-live"></span>
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <div class="xp-social-profile-menu text-right py-3">
+                                                <i class="mdi mdi-dots-horizontal font-24"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="xp-social-profile-middle text-center">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="xp-social-profile-title">
+                                                <h5 class="my-1 text-black">Dimas_Shogun</h5>
+                                            </div>
+                                            <div class="xp-social-profile-subtitle">
+                                                <p class="mb-3 text-muted">Dimas Shofa Gunarso</p>
+                                            </div>
+                                            <div class="xp-social-profile-desc">
+                                                <p class="text-muted">"Jangan takut berjalan lambat, takutlah jika hanya berdiri diam." <br>Kelas 6B Nim 18090061</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="xp-social-profile-bottom text-center">
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <div class="xp-social-profile-media pt-3">
+                                                <h5 class="text-black my-1">34</h5>
+                                                <p class="mb-0 text-muted">Posts</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="xp-social-profile-followers pt-3">
+                                                <h5 class="text-black my-1">378k</h5>
+                                                <p class="mb-0 text-muted">Fans</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="xp-social-profile-following pt-3">
+                                                <h5 class="text-black my-1">1k</h5>
+                                                <p class="mb-0 text-muted">Likes</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </center>
+
+            <!-- Optional JavaScript; choose one of the two! -->
+
+            <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
